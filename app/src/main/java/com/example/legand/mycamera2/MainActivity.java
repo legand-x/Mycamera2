f6e36852654f38f3c3410a8770008f3f1e135976
Legand <angel_909_14@hotmail.com>
2019-04-29 16:22:01 -0500

    new fixes to AndroidManifest.xml and refactor to MainActivity.class

--- app/src/main/java/com/example/legand/mycamera2/MainActivity.java
+++ app/src/main/java/com/example/legand/mycamera2/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.legand.mycamera2;
 
 import android.app.Activity;
 import android.arch.lifecycle.ViewModelProvider;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.frosquivel.magicalcamera.MagicalCamera;
 import com.frosquivel.magicalcamera.MagicalPermissions;
 
+import java.util.Map;
 import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,27 +23,31 @@ public class MainActivity extends AppCompatActivity {
     private MagicalPermissions magicalPermissions;
     private ImageView ImageViewFoto;
     private MagicalCamera magicalCamera;
-
+    private Button photoButton;
+    String[] permissions = new String[] {
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageViewFoto = findViewById(R.id.imageViewFoto);
+        photoButton = findViewById(R.id.buttonFoto);
 
-        findViewById(R.id.buttonFoto).setOnClickListener(this);
-        ImageViewFoto=findViewById(R.id.imageViewFoto);
-
-
-        String[] permissions = new String[] {
-                Manifest.permission.CAMERA,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        };
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                magicalCamera.takePhoto();
+            }
+        });
+        //findViewById(R.id.buttonFoto).setOnClickListener(this);
         magicalPermissions= new MagicalPermissions(this, permissions);
         magicalCamera=new MagicalCamera(this,RESIZE_PHOTO_PIXELS_PORCENTAGE,magicalPermissions);
     }
 
-    public void onClick(View v) {magicalCamera.takePhoto();}
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         Map<String, Boolean> map = magicalPermissions.permissionResult(requestCode, permissions, grantResults);
@@ -52,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        String path  = null;
         //CALL THIS METHOD EVER
         if (resultCode==RESULT_OK){
         magicalCamera.resultPhoto(requestCode, resultCode, data);
@@ -64,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
         //if you need save your bitmap in device use this method and return the path if you need this
         //You need to send, the bitmap picture, the photo name, the directory name, the picture type, and autoincrement photo name if           //you need this send true, else you have the posibility or realize your standard name for your pictures.
-        String path = magicalCamera.savePhotoInMemoryDevice(magicalCamera.getPhoto(), "myPhotoName", "myDirectoryName", MagicalCamera.JPEG, true);
+        path = magicalCamera.savePhotoInMemoryDevice(magicalCamera.getPhoto(), "myPhotoName", "myDirectoryName", MagicalCamera.JPEG, true);
     }
         if(path != null){
             Toast.makeText(MainActivity.this, "The photo is save in device, please check this path: " + path, Toast.LENGTH_SHORT).show();
